e6ac7175c1e786ec5aea74946fcb55c962076ff6
freeaircn <freeaircn@163.com>
2019-02-21 00:30:16 +0800

    updated

--- client/src/views/home/index.vue
+++ client/src/views/home/index.vue
@@ -1,40 +1,26 @@
 <template>
-  <div class="login-container">
-    <h3 class="title">Hello World</h3>
+  <div class="home-container">
+    <h3 class="title">Home Page</h3>
   </div>
 </template>
 
 <script>
-
 export default {
-  name: 'Login',
-  data() {
-    return {
-      redirect: undefined
-    }
-  },
-  watch: {
-    $route: {
-      handler: function(route) {
-        this.redirect = route.query && route.query.redirect
-      },
-      immediate: true
-    }
-  }
+  name: 'Home'
 }
 </script>
 
 <style rel="stylesheet/scss" lang="scss">
-$bg:#e4e5e7;
+$bg:#2d3a4b;
 $light_gray:#eee;
 
 </style>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-$bg:#e4e5e7;
+$bg:#2d3a4b;
 $dark_gray:#889aa4;
 $light_gray:#eee;
-.login-container {
+.home-container {
   position: fixed;
   height: 100%;
   width: 100%;
